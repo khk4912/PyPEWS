89c8fc067d163543fcbad244587f20ddb7cc183b
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-09-15 14:25:37 +0900

    asfd

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 import datetime
 
+timeZone = 9
+tzMsec = timeZone * 3600000
+tide = delay = 1000
+DATA_PATH = "https://www.weather.go.kr/pews/data"
 
-def get_MMI(url: str):
-    r = requests.get(url, timeout=1.0)
 
-    if r.status_code == 200:
-        pass
+def get_MMI() -> bytes:
+    pTime = datetime.datetime.now().strftime("%Y%m%H%M%S")
+    r = requests.get(f"{DATA_PATH}/{pTime}.b", timeout=1.0)
+
+    if (status := r.status_code) == 200:
+        return r.content
     else:
-        pass
+        raise ZeroDivisionError(str(status))  # XXX : 임시
+
